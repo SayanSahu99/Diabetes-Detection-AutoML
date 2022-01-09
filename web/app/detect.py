5a616b24c791cfeb13cf260024ec7c9211a977d7
SayanSahu99 <sayansovan@gmail.com>
2022-01-09 20:19:14 +0530

    detect.py to consume.py

---
+++ web/app/detect.py
@@ -0,0 +1,32 @@
+import urllib.request
+import json
+import os
+import ssl
+
+def allowSelfSignedHttps(allowed):
+    # bypass the server certificate verification on client side
+    if allowed and not os.environ.get('PYTHONHTTPSVERIFY', '') and getattr(ssl, '_create_unverified_context', None):
+        ssl._create_default_https_context = ssl._create_unverified_context
+
+def detect_diabetes(data):
+    allowSelfSignedHttps(True) # this line is needed if you use self-signed certificate in your scoring service
+    
+    body = str.encode(json.dumps(data))
+
+    url = 'http://903ad508-e251-4475-8ae8-155a68dc106b.eastus.azurecontainer.io/score'
+    api_key = '' # Replace this with the API key for the web service
+    headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
+
+    req = urllib.request.Request(url, body, headers)
+
+    try:
+        response = urllib.request.urlopen(req)
+
+        result = response.read()
+        return result
+    except urllib.error.HTTPError as error:
+        print("The request failed with status code: " + str(error.code))
+
+        # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
+        print(error.info())
+        return None
